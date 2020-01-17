d7d402c5bdcbbd40dbfb72779a1fb544eb9f4dd1
Amelia Galgon <agalgon@gmail.com>
2020-01-17 15:46:01 -0500

    add collections widget

--- rachael-morgan-jewelry-plugin.php
+++ rachael-morgan-jewelry-plugin.php
@@ -8,6 +8,7 @@ Description: Site specific code changes for rachaelmorganjewelry.com
 // Register and load the widget
 function wpb_load_widget() {
     register_widget( 'rachael_morgan_jewelry_landing_widget' );
+    register_widget('rachael_morgan_jewelry_collection_widget');
 }
 add_action( 'widgets_init', 'wpb_load_widget' );
 
@@ -19,7 +20,7 @@ function wpb_load_script() {
     wp_enqueue_script('ads_script', plugin_dir_url( __FILE__ ) . '/js/widget.js', false, '1.0.0', true);
 }
 
-// Creating the widget
+// Creating the landing widget
 class rachael_morgan_jewelry_landing_widget extends WP_Widget {
 
     function __construct() {
@@ -42,80 +43,37 @@ class rachael_morgan_jewelry_landing_widget extends WP_Widget {
     // Widget Backend
     public function form( $instance ) {
 
-        $defaults = array('title'   => '',
-            'text'      => '',
-            'textarea'  => '',
-            'checkbox'  => '',
-            'select'    => '',
+        $defaults = array(
+            'logo_uri'      => '',
             'image_uri'     => ''
         );
 
         //extract( wp_parse_args($instance, $defaults));
         extract( wp_parse_args( ( array ) $instance, $defaults ) );
 
-        // Widget admin form
-        ?>
+        // Logo ?>
         <p>
-            <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Widget Title', 'text_domain' ); ?></label>
-            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+            <label for="<?= $this->get_field_id( 'logo_uri' ); ?>">Image</label>
+            <img class="<?= $this->id ?>-logo_img" src="<?= (!empty($instance['logo_uri'])) ? $instance['logo_uri'] : ''; ?>" style="margin:0;padding:0;max-width:100%;display:block"/>
+            <input type="text" class="widefat <?= $this->id ?>-logo_url" name="<?= $this->get_field_name( 'logo_uri' ); ?>" value="<?= $instance['logo_uri']; ?>" style="margin-top:5px;" />
+            <input type="button" id="<?= $this->id ?>-logo" class="button button-primary js_custom_upload_media" value="Upload Logo" style="margin-top:5px;" />
         </p>
 
-        <?php // Text Field ?>
-        <p>
-            <label for="<?php echo esc_attr( $this->get_field_id( 'text' ) ); ?>"><?php _e( 'Text:', 'text_domain' ); ?></label>
-            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'text' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'text' ) ); ?>" type="text" value="<?php echo esc_attr( $text ); ?>" />
-        </p>
-
-        <?php // Textarea Field ?>
-        <p>
-            <label for="<?php echo esc_attr( $this->get_field_id( 'textarea' ) ); ?>"><?php _e( 'Textarea:', 'text_domain' ); ?></label>
-            <textarea class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'textarea' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'textarea' ) ); ?>"><?php echo wp_kses_post( $textarea ); ?></textarea>
-        </p>
-
-        <?php // Checkbox ?>
-        <p>
-            <input id="<?php echo esc_attr( $this->get_field_id( 'checkbox' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'checkbox' ) ); ?>" type="checkbox" value="1" <?php checked( '1', $checkbox ); ?> />
-            <label for="<?php echo esc_attr( $this->get_field_id( 'checkbox' ) ); ?>"><?php _e( 'Checkbox', 'text_domain' ); ?></label>
-        </p>
-
-        <?php // Dropdown ?>
-        <p>
-            <label for="<?php echo $this->get_field_id( 'select' ); ?>"><?php _e( 'Select', 'text_domain' ); ?></label>
-            <select name="<?php echo $this->get_field_name( 'select' ); ?>" id="<?php echo $this->get_field_id( 'select' ); ?>" class="widefat">
-                <?php
-                // Your options array
-                $options = array(
-                    ''        => __( 'Select', 'text_domain' ),
-                    'option_1' => __( 'Option 1', 'text_domain' ),
-                    'option_2' => __( 'Option 2', 'text_domain' ),
-                    'option_3' => __( 'Option 3', 'text_domain' ),
-                );
-
-                // Loop through options and add each one to the select dropdown
-                foreach ( $options as $key => $name ) {
-                    echo '<option value="' . esc_attr( $key ) . '" id="' . esc_attr( $key ) . '" '. selected( $select, $key, false ) . '>'. $name . '</option>';
-
-                } ?>
-            </select>
-        </p>
         <?php // Image ?>
         <p>
             <label for="<?= $this->get_field_id( 'image_uri' ); ?>">Image</label>
-            <img class="<?= $this->id ?>_img" src="<?= (!empty($instance['image_uri'])) ? $instance['image_uri'] : ''; ?>" style="margin:0;padding:0;max-width:100%;display:block"/>
-            <input type="text" class="widefat <?= $this->id ?>_url" name="<?= $this->get_field_name( 'image_uri' ); ?>" value="<?= $instance['image_uri']; ?>" style="margin-top:5px;" />
-            <input type="button" id="<?= $this->id ?>" class="button button-primary js_custom_upload_media" value="Upload Image" style="margin-top:5px;" />
+            <img class="<?= $this->id ?>-image_img" src="<?= (!empty($instance['image_uri'])) ? $instance['image_uri'] : ''; ?>" style="margin:0;padding:0;max-width:100%;display:block"/>
+            <input type="text" class="widefat <?= $this->id ?>-image_url" name="<?= $this->get_field_name( 'image_uri' ); ?>" value="<?= $instance['image_uri']; ?>" style="margin-top:5px;" />
+            <input type="button" id="<?= $this->id ?>-image" class="button button-primary js_custom_upload_media" value="Upload Image" style="margin-top:5px;" />
         </p>
+
     <?php
     }
 
     // Update widget settings
     public function update( $new_instance, $old_instance ) {
         $instance = $old_instance;
-        $instance['title']    = isset( $new_instance['title'] ) ? wp_strip_all_tags( $new_instance['title'] ) : '';
-        $instance['text']     = isset( $new_instance['text'] ) ? wp_strip_all_tags( $new_instance['text'] ) : '';
-        $instance['textarea'] = isset( $new_instance['textarea'] ) ? wp_kses_post( $new_instance['textarea'] ) : '';
-        $instance['checkbox'] = isset( $new_instance['checkbox'] ) ? 1 : false;
-        $instance['select']   = isset( $new_instance['select'] ) ? wp_strip_all_tags( $new_instance['select'] ) : '';
+        $instance['logo_uri'] = wp_strip_all_tags( $new_instance['logo_uri'] );
         $instance['image_uri'] = wp_strip_all_tags( $new_instance['image_uri'] );
         return $instance;
     }
@@ -126,12 +84,116 @@ class rachael_morgan_jewelry_landing_widget extends WP_Widget {
         extract( $args );
 
         // Check the widget options
-        $title      = isset( $instance['title'] ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
-        $text       = isset( $instance['text'] ) ? $instance['text'] : '';
-        $textarea   = isset( $instance['textarea'] ) ?$instance['textarea'] : '';
-        $select     = isset( $instance['select'] ) ? $instance['select'] : '';
-        $checkbox   = ! empty( $instance['checkbox'] ) ? $instance['checkbox'] : false;
         $image_uri  = isset( $instance['image_uri'] ) ? $instance['image_uri'] : '';
+        $logo_uri   = isset( $instance['logo_uri'] ) ? $instance['logo_uri'] : '';
+
+        // WordPress core before_widget hook (always include )
+        echo $before_widget;
+
+        // Display the widget
+        echo '<div class="widget-text wp_widget_plugin_box">';
+
+        if ( $logo_uri )?>
+            <img src="<?php echo esc_url($instance['logo_uri']); ?>" />
+        <?php
+
+        if ( $image_uri )?>
+            <img src="<?php echo esc_url($instance['image_uri']); ?>" />
+        <?php
+
+
+        echo '</div>';
+
+        // WordPress core after_widget hook (always include )
+        echo $after_widget;
+
+    }
+
+} // Class wpb_widget ends here
+
+class rachael_morgan_jewelry_collection_widget extends WP_Widget {
+
+    function __construct() {
+        parent::__construct(
+
+        // Base ID of your widget
+            'rachael_morgan_jewelry_collection_widget',
+
+            // Widget name will appear in UI
+            __('Rachael Morgan Jewelry Collection Widget', 'text_domain'),
+
+            // Widget description
+            array ('description'                 => __( 'Sample widget based on WPBeginner Tutorial', 'text_domain' ),
+                'customize_selective_refresh'   => true,
+                'classname'                     => 'widget_rachael-morgan-jewelry-collection'
+            )
+        );
+    }
+
+    // Widget Backend
+    public function form( $instance ) {
+
+        $defaults = array(
+            'title'         => '',
+            'description'   => '',
+            'link'          => '',
+            'image_uri'     => ''
+        );
+
+        //extract( wp_parse_args($instance, $defaults));
+        extract( wp_parse_args( ( array ) $instance, $defaults ) );
+
+        // Title ?>
+
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Title', 'text_domain' ); ?></label>
+            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+        </p>
+
+        <?php // Description ?>
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'description' ) ); ?>"><?php _e( 'Description:', 'text_domain' ); ?></label>
+            <textarea class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'description' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'description' ) ); ?>"><?php echo wp_kses_post( $textarea ); ?></textarea>
+        </p>
+
+        <?php // Link ?>
+
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'link' ) ); ?>"><?php _e( 'Link', 'text_domain' ); ?></label>
+            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'link' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'link' ) ); ?>" type="text" value="<?php echo esc_attr( $link ); ?>" />
+        </p>
+
+        <?php // Image ?>
+        <p>
+            <label for="<?= $this->get_field_id( 'image_uri' ); ?>">Image</label>
+            <img class="<?= $this->id ?>-image_url" src="<?= (!empty($instance['image_uri'])) ? $instance['image_uri'] : ''; ?>" style="margin:0;padding:0;max-width:100%;display:block"/>
+            <input type="text" class="widefat <?= $this->id ?>-image_url" name="<?= $this->get_field_name( 'image_uri' ); ?>" value="<?= $instance['image_uri']; ?>" style="margin-top:5px;" />
+            <input type="button" id="<?= $this->id ?>-image" class="button button-primary js_custom_upload_media" value="Upload Image" style="margin-top:5px;" />
+        </p>
+
+        <?php
+    }
+
+    // Update widget settings
+    public function update( $new_instance, $old_instance ) {
+        $instance = $old_instance;
+        $instance['title']    = isset( $new_instance['title'] ) ? wp_strip_all_tags( $new_instance['title'] ) : '';
+        $instance['link']     = isset( $new_instance['link'] ) ? wp_strip_all_tags( $new_instance['link'] ) : '';
+        $instance['description'] = isset( $new_instance['description'] ) ? wp_kses_post( $new_instance['description'] ) : '';
+        $instance['image_uri'] = wp_strip_all_tags( $new_instance['image_uri'] );
+        return $instance;
+    }
+
+    // Display the widget
+    public function widget( $args, $instance ) {
+
+        extract( $args );
+
+        // Check the widget options
+        $title          = isset( $instance['title'] ) ? $instance['title'] : '';
+        $description    = isset( $instance['description'] ) ? $instance['description'] : '';
+        $link           = isset( $instance['link'] ) ? $instance['link'] : '';
+        $image_uri      = isset( $instance['image_uri'] ) ? $instance['image_uri'] : '';
 
         // WordPress core before_widget hook (always include )
         echo $before_widget;
@@ -144,25 +206,18 @@ class rachael_morgan_jewelry_landing_widget extends WP_Widget {
             echo $before_title . $title . $after_title;
         }
 
-        // Display text field
-        if ( $text ) {
-            echo '<p>' . $text . '</p>';
-        }
-
         // Display textarea field
-        if ( $textarea ) {
-            echo '<p>' . $textarea . '</p>';
+        if ( $description ) {
+            echo '<p>' . $description . '</p>';
         }
 
-        // Display select field
-        if ( $select ) {
-            echo '<p>' . $select . '</p>';
+        if ( $link ) {
+            echo '<a href="' . $link . '"><p>' . $link . '</p></a>';
         }
 
-        // Display something if checkbox is true
-        if ( $checkbox ) {
-            echo '<p>Something awesome</p>';
-        }
+        if ( $logo_uri )?>
+            <img src="<?php echo esc_url($instance['logo_uri']); ?>" />
+        <?php
 
         if ( $image_uri )?>
             <img src="<?php echo esc_url($instance['image_uri']); ?>" />
